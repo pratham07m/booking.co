fcfa104a76000f8ed58f245cb2c78c53c22bd77b
Pratham_Mahant <pratham2004m@gmail.com>
2025-04-28 22:42:04 +0530

    get all bookings and cancel bookings added

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// user book a slot
 export const bookSlot = async (req, res) => {
   try {
     if (req.user.role !== "USER") {
@@ -26,5 +27,57 @@ export const bookSlot = async (req, res) => {
     res.status(201).json({ message: "booking successful", booking });
   } catch (error) {
     return res.status(500).json({ message: "Internal server Error" });
+  }
+};
+
+// get all user booking slots
+export const getuserBookings = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const bookings = await prisma.booking.findMany({
+      where: { userId },
+      include: {
+        slot: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    res.status(200).json({ bookings });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// cancel booking slot
+export const cancelBooking = async (req, res) => {
+  try {
+    const bookingId = req.params.bookingId;
+    const userId = req.user.id;
+    
+    // find bookings
+    const booking = await prisma.booking.findUnique({
+      where: { id: Number(bookingId) },
+    });
+
+    if (!bookingId)
+      return res.status(404).json({ message: "booking not found" });
+
+    //check only user book user can cancel slot
+    if (booking.userId !== userId)
+      return res
+        .ststus(403)
+        .json({ message: "you cannot cancel this booking" });
+
+    // delete booking
+    await prisma.booking.delete({
+      where: { id: Number(bookingId) },
+    });
+
+    res.status(200).json({ message: "booking cancelld successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "internal server error" });
   }
 };
--- src/routes/booking.routes.js
+++ src/routes/booking.routes.js
@@ -1,9 +1,17 @@
-import express from "express"
-import { bookSlot } from "../controllers/booking.controller.js"
-import { authenticate } from "../middlewares/auth.middlewares.js"
+import express from "express";
+import {
+  bookSlot,
+  getuserBookings,
+  cancelBooking,
+} from "../controllers/booking.controller.js";
+import { authenticate } from "../middlewares/auth.middlewares.js";
 
 const router = express.Router();
 
-router.post("/book" , authenticate , bookSlot);
+router.post("/book", authenticate, bookSlot);
 
-export default router
+router.get("/my-bookings", authenticate, getuserBookings);
+
+router.delete("/cancel/:bookingId", authenticate , cancelBooking);
+
+export default router;
